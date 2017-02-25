9a378bdb0ee217831a9cd440be6b835dc18aefa1
Will Dietz <w@wdtz.org>
2017-02-25 14:32:26 -0600

    Decompose: update-format

--- tools/allplay/Decompose.cpp
+++ tools/allplay/Decompose.cpp
@@ -1,18 +1,18 @@
 #include "subcommand-registry.h"
 
 #include <llvm/ADT/StringExtras.h>
+#include <llvm/Bitcode/BitcodeReader.h>
+#include <llvm/Bitcode/BitcodeWriter.h>
 #include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
 #include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/Errc.h>
+#include <llvm/Support/Error.h>
 #include <llvm/Support/FileSystem.h>
 #include <llvm/Support/SourceMgr.h>
 #include <llvm/Support/ToolOutputFile.h>
 #include <llvm/Transforms/Utils/SplitModule.h>
-#include <llvm/Bitcode/BitcodeReader.h>
-#include <llvm/Bitcode/BitcodeWriter.h>
-#include <llvm/IR/Module.h>
-#include <llvm/Support/Errc.h>
-#include <llvm/Support/Error.h>
-#include <llvm/IR/LegacyPassManager.h>
 
 using namespace allvm;
 using namespace llvm;
@@ -21,13 +21,13 @@ namespace {
 
 // TODO: 'decompose' allexe in-place?
 cl::SubCommand Decompose("decompose",
-    "Decompose a bitcode file into linkable fragments");
+                         "Decompose a bitcode file into linkable fragments");
 cl::opt<std::string> InputFile(cl::Positional, cl::Required,
-    cl::desc("input bitcode filename"),
-    cl::sub(Decompose));
+                               cl::desc("input bitcode filename"),
+                               cl::sub(Decompose));
 cl::opt<std::string> OutputDirectory("o", cl::Required,
-    cl::desc("Path to write fragments"),
-    cl::sub(Decompose));
+                                     cl::desc("Path to write fragments"),
+                                     cl::sub(Decompose));
 
 // This snippet taken from ThinLTOBitcodeWriter, repurposed:
 // Even modules that don't export could have initializers..
@@ -40,7 +40,7 @@ bool exportsSymbol(llvm::Module *M) {
     ExportsSymbols = true;
   };
 
-	// TODO: Early exit would be nice :P
+  // TODO: Early exit would be nice :P
   for (auto &F : *M)
     AddGlobal(F);
   for (auto &GV : M->globals())
@@ -50,7 +50,7 @@ bool exportsSymbol(llvm::Module *M) {
   for (auto &IF : M->ifuncs())
     AddGlobal(IF);
 
-	return ExportsSymbols;
+  return ExportsSymbols;
 }
 
 Error decompose(StringRef BCFile, StringRef OutDir) {
@@ -58,7 +58,8 @@ Error decompose(StringRef BCFile, StringRef OutDir) {
   SMDiagnostic Diag;
   auto M = parseIRFile(BCFile, Diag, C);
   if (!M)
-    return make_error<StringError>("Unable to open IR file " + BCFile, errc::invalid_argument);
+    return make_error<StringError>("Unable to open IR file " + BCFile,
+                                   errc::invalid_argument);
 
   if (auto Err = M->materializeAll())
     return Err;
@@ -67,19 +68,21 @@ Error decompose(StringRef BCFile, StringRef OutDir) {
   // using SplitModule and whatnot.  But it's a start.
   unsigned NumOutputs = 1000; // M->global_objects());
   std::vector<std::unique_ptr<Module>> Parts;
-  SplitModule(std::move(M), NumOutputs, [&](std::unique_ptr<Module> MPart) {
-			Parts.emplace_back(std::move(MPart));
-  }, false /* PreserveLocals */);
+  SplitModule(std::move(M), NumOutputs,
+              [&](std::unique_ptr<Module> MPart) {
+                Parts.emplace_back(std::move(MPart));
+              },
+              false /* PreserveLocals */);
 
-	// TODO: Filter out useless modules
-	// TODO: global-opt?
+  // TODO: Filter out useless modules
+  // TODO: global-opt?
 
-	size_t ModsWithoutExports = 0;
+  size_t ModsWithoutExports = 0;
   for (auto &MPart : Parts) {
-		if (!exportsSymbol(MPart.get()))
-			++ModsWithoutExports;
-	}
-	errs() << "ModsWithoutExports: "<< ModsWithoutExports << "\n";
+    if (!exportsSymbol(MPart.get()))
+      ++ModsWithoutExports;
+  }
+  errs() << "ModsWithoutExports: " << ModsWithoutExports << "\n";
 
   if (auto EC = sys::fs::create_directories(OutDir))
     return errorCodeToError(EC);
@@ -88,8 +91,7 @@ Error decompose(StringRef BCFile, StringRef OutDir) {
   for (auto &MPart : Parts) {
     std::error_code EC;
     std::string OutName = (OutDir + "/" + utostr(I++)).str();
-    tool_output_file Out(OutName, EC,
-        sys::fs::F_None);
+    tool_output_file Out(OutName, EC, sys::fs::F_None);
     if (EC)
       return errorCodeToError(EC);
 
@@ -101,8 +103,9 @@ Error decompose(StringRef BCFile, StringRef OutDir) {
   return Error::success();
 }
 
-CommandRegistration Unused(&Decompose, [](ResourcePaths &RP LLVM_ATTRIBUTE_UNUSED) -> Error {
-    return decompose(InputFile, OutputDirectory);
-});
+CommandRegistration
+    Unused(&Decompose, [](ResourcePaths &RP LLVM_ATTRIBUTE_UNUSED) -> Error {
+      return decompose(InputFile, OutputDirectory);
+    });
 
 } // end anonymous namespace
