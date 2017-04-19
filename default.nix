7cd2683043aa9c24bf5f22d35d5ac0675dd3ca5c
Will Dietz <w@wdtz.org>
2017-04-19 08:51:06 -0500

    simplify nix expr's, invoke default.nix from release.nix

--- default.nix
+++ default.nix
@@ -2,32 +2,16 @@ let
   default_nixpkgs = (import <nixpkgs> {}).fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs-channels";
-    rev = "f0fac3b578086066b47360de17618448d066b30e"; # current -unstable
-    sha256 = "1mpwdminwk1wzycwmgi2c2kwpbcfjwmxiakn7bmvvsaxb30gwyyb";
+    rev = "ab1078806ecf7f1ef28b3a0bd7cda1e9af8e7875"; # current 17.03 beta
+    sha256 = "09xm5i7bghb8ndihcsvnc1r42inpv6wzwhk6rdl75hpzhr290s18";
   };
 in
 { nixpkgs ? default_nixpkgs }:
 
 with import nixpkgs {};
-let
-  gcc_stdenv = overrideCC stdenv gcc-snapshot;
-  clang_stdenv = llvmPackages_4.stdenv;
-in rec {
-  # Nope, still doesn't work :(
-  # allvm-tools-gcc = callPackage ./build.nix {
-  #   inherit (llvmPackages_4) llvm clang lld;
-  #   stdenv = gcc_stdenv;
-  #   rangev3 = callPackage ./rangev3.nix {
-  #     stdenv = gcc_stdenv;
-  #   };
-  # };
-
-  allvm-tools-clang = callPackage ./build.nix {
-    inherit (llvmPackages_4) llvm clang lld;
-    stdenv = clang_stdenv;
-    rangev3 = callPackage ./rangev3.nix {
-      stdenv = clang_stdenv;
-    };
+callPackage ./build.nix {
+  inherit (llvmPackages_4) stdenv llvm clang lld;
+  rangev3 = callPackage ./rangev3.nix {
+    stdenv = llvmPackages_4.stdenv;
   };
-
 }
