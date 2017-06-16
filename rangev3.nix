2af91dac4a2f0b280613a96d5fff0b74b6ae6972
Will Dietz <w@wdtz.org>
2017-06-15 19:18:57 -0500

    rangev3: 2.4 -> 2.5

--- rangev3.nix
+++ rangev3.nix
@@ -4,13 +4,13 @@ let
   isMusl = stdenv.isMusl or false;
 in stdenv.mkDerivation rec {
   name = "range-v3-${version}";
-  version = "2.4";
+  version = "2.5";
 
   src = fetchFromGitHub {
     owner = "ericniebler";
     repo = "range-v3";
-    rev = "d753c6652f3bd1bb9fac42cfbf11fd55d856f97d";
-    sha256 = "0qxmxqclq8nn5b5d1z8dfy5sw209ch6lxgfqmnsxmjwr6ms6v67z";
+    rev = "8e12b0ea21b4e2c57fd0e77726b0e51b96e6a8b6";
+    sha256 = "14avawj4ycnff5cz7gwvcr19abahlhrv2b41glkmahj16an04qjr";
   };
 
   buildInputs = [ cmake ];
