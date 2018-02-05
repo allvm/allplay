6887570b9e5a2154732d10686c0cc608a05f595f
Will Dietz <w@wdtz.org>
2018-02-05 10:45:58 -0600

    nix: fix dropped char, whoops

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 {
 nixpkgs ? import ./nix/fetch-nixpkgs.nix,
-allvm-tools-src ? builtins.fetchGit { url ./.; ref = "master"; },
+allvm-tools-src ? builtins.fetchGit { url = ./.; ref = "master"; },
 allvm-tools ? (import allvm-tools-src {}).allvm-tools-clang
 }:
 
